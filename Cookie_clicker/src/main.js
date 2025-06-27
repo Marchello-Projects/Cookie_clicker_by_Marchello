c22ab71b075e67817d569ab0207ac52a300026a5
Marchello <paskalovmarkus@gmail.com>
2025-06-27 22:39:57 +0300

    HTML & SCSS

---
+++ Cookie_clicker/src/main.js
@@ -0,0 +1,5 @@
+import { alert, error } from "@pnotify/core"
+import "@pnotify/core/dist/PNotify.css"
+import "@pnotify/core/dist/BrightTheme.css"
+import './styles/reset.scss'
+import './styles/style.scss'
